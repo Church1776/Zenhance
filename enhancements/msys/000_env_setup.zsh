1dd09cc57882eddfdec15c944fb31e66c40e50eb
Church1776 <brassmajor@gmail.com>
2026-06-01 10:29:07 -0400

    Updated msys environment file

--- enhancements/msys/000_env_setup.zsh
+++ enhancements/msys/000_env_setup.zsh
@@ -45,3 +45,62 @@ function javapath {
 }
 javapath
 unset -f javapath
+
+CC="clang"
+CCSTD="c23"
+
+CXX="clang++"
+CXXSTD="c++23"
+CXXSTDLIB="libc++"
+CXXRTLIB="compiler-rt"
+CXXUSELD="lld"
+
+function incfinder {
+REPO_ROOT="$(git rev-parse --show-toplevel 2> /dev/null)"
+
+TEMPPWD="$PWD"
+
+while [[ "$TEMPPWD" == "$REPO_ROOT/*" || "$TEMPPWD" == "$REPO_ROOT" ]]; do
+  printf "Checking \033[33m$TEMPPWD\033[0m for inc directory...\n"
+  CXXINCLUDE="$(find "$TEMPPWD" -maxdepth 1 -type d -name "inc"* -print -quit)"
+  if [[ -d "$CXXINCLUDE" ]]; then
+    printf "Found inc directory at \033[32m$CXXINCLUDE\033[0m\n"
+  fi
+  TEMPPWD="${TEMPPWD:h}"
+done
+
+unset REPO_ROOT
+unset TEMPPWD
+}
+incfinder
+
+export CC CCSTD CXX CXXSTD CXXSTDLIB CXXRTLIB CXXUSELD CXXINCLUDE
+
+WINLIBS=(-luser32 -lgdi32)
+
+export WINLIBS
+
+function ccmpl {
+  $CC -std=$CCSTD -I$CXXINCLUDE "$@" ${WINLIBS[@]}
+}
+
+function cxxcmpl {
+  $CXX -std=$CXXSTD -stdlib=$CXXSTDLIB -rtlib=$CXXRTLIB -fuse-ld=$CXXUSELD -I$CXXINCLUDE "$@" ${WINLIBS[@]}
+}
+
+function set_cmpl {
+  
+  case "$1" in
+    c|C) alias cmpl=ccmpl;;
+    c++|C++|cxx|CXX|cpp|CPP) alias cmpl=cxxcmpl;;
+    *) echo "Unknown Argument: $1"
+      echo "Available options: c, C, c++, C++, cxx, CXX, cpp, CPP"
+      ;;
+  esac
+
+  if [[ -z $(alias cmpl 2>/dev/null) ]]; then
+    alias cmpl=cxxcmpl
+  fi
+}
+set_cmpl
+
