28b4a86479e03d8ef0e0b03f0babee880888e51d
Church1776 <brassmajor@gmail.com>
2026-09-23 16:55:38 -0400

    Configured interactive_cd: Now Use: <-, ->, & 'enter' to move between directories interactively.

--- enhancements/extra_widgets.zsh
+++ enhancements/extra_widgets.zsh
@@ -1,13 +1,96 @@
 #List of functions to create custom zle widgets and bind keys to them.
 function declare_custom_widgets {
-  toggle_overwrite_mode() {
-    zle overwrite-mode
-    if [[ $ZLE_STATE == *"overwrite"* ]]; then
-      echo -ne '\e[1 q\e[?12h'  # Blinking block
-    else
-      echo -ne '\e[3 q\e[?12h'  # Blinking underline
-    fi
+  overwrite_mode() { zle overwrite-mode; [[ $ZLE_STATE == *"overwrite"* ]] && { echo -ne '\e[1 q\e[?12h'; return; }; echo -ne '\e[3 q\e[?12h'; }
+  
+interactive_cd() {
+  local root="$PWD"
+  local level=0
+  local display=""
+  local response action chosen query
+  local -a response_lines
+  local fzf_cmd=${FZF_BASE:-fzf}
+
+  [[ $LBUFFER == cd || $LBUFFER == 'cd '* ]] || {
+    zle .expand-or-complete
+    return
   }
+
+  query=${LBUFFER#cd }
+  zle -I
+
+  while true; do
+    if ! response=$(
+      {
+        print -r -- .
+        fd --type d --hidden --follow --max-depth 1
+      } |
+        "$fzf_cmd" \
+          --height=40% \
+          --layout=reverse \
+          --scheme=path \
+          --prompt="${level}: ${display}> " \
+          --query="${query}" \
+          +m \
+          --bind='enter:become(printf "accept\n%s\n" {})' \
+          --bind='right:become(printf "descend\n%s\n" {})' \
+          --bind='left:become(printf parent)'
+    ); then
+      zle reset-prompt
+      return
+    fi
+
+    response_lines=("${(@f)response}")
+    action=${response_lines[1]}
+    chosen=${response_lines[2]}
+
+    case $action in
+      descend)
+        cd -- "$chosen" 2>/dev/null || continue
+        root="$PWD"
+        (( --level ))
+        if (( $level > 0 )); then
+          display="${display%/*}"
+          display="${display%/*}"
+          display="${display:+$display/}"
+        elif (( $level == 0 )); then
+          display=''
+        else
+          display+="$chosen"
+        fi
+        ;;
+      parent)
+        if [[ ! $root == / ]]; then
+          cd -- ".." 2>/dev/null || continue
+          root="$PWD"
+          (( ++level ))
+          if (( $level < 0 )); then
+            display="${display%/*}"
+            display="${display%/*}"
+            display="${display:+$display/}"
+          elif (( $level == 0 )); then
+            display=''
+          else
+            display+='../'
+          fi
+        fi
+        ;;
+      accept)
+        [[ $chosen == . ]] && chosen=$root
+        [[ -n $chosen && -d $chosen ]] || return
+
+        cd -- "$chosen" || return
+        BUFFER=
+        CURSOR=0
+        precmd
+        zle reset-prompt
+        return
+        ;;
+    esac
+
+    query=
+  done
+}
+
   backward_char() {(( REGION_ACTIVE )) && {(( CURSOR > MARK )) && { CURSOR=$MARK;}; zle deactivate-region; return;}; zle backward-char;}
   select_backward_char() {(( REGION_ACTIVE )) || zle set-mark-command; zle backward-char;}
 
@@ -23,8 +106,8 @@ function declare_custom_widgets {
   delete_char() {(( REGION_ACTIVE )) && zle kill-region; zle delete-char;}
   delete_word() {(( REGION_ACTIVE )) && zle kill-region; zle delete-word;}
 
-  backward_delete_char() {(( REGION_ACTIVE )) && zle kill-region; zle backward-delete-char;}
-  backward_delete_word() {(( REGION_ACTIVE )) && zle kill-region; zle backward-delete-word;}
+  backward_delete_char() {(( REGION_ACTIVE )) && { zle kill-region; return; }; zle backward-delete-char;}
+  backward_delete_word() {(( REGION_ACTIVE )) && { zle kill-region; return; }; zle backward-delete-word;}
 
   wrap_region() {
     local open="$1"
@@ -42,11 +125,13 @@ function declare_custom_widgets {
   wrap_parens() { wrap_region '(' ')'; }
   wrap_brackets() { wrap_region '[' ']'; }
   wrap_braces() { wrap_region '{' '}'; }
+
+  show_keys() { zle -M "received: ${(q-)KEYS}"; }
 }
 declare_custom_widgets
 
 function create_zle_custom_widgets { 
-  zle -N toggle_overwrite_mode
+  zle -N overwrite_mode
 
   zle -N delete_char
   zle -N backward_delete_char
@@ -70,6 +155,10 @@ function create_zle_custom_widgets {
   zle -N wrap_parens
   zle -N wrap_brackets
   zle -N wrap_braces
+
+  zle -N show_keys
+
+  zle -N interactive_cd
 }
 create_zle_custom_widgets
 
